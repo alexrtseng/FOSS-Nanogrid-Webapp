4b1b1c18dc6327a2a38ac5718452eada71cbf952
alexrtseng <at2720@princeton.edu>
2024-07-10 15:32:47 +0300

    Getting app started

--- foss_nanogrid/optimization/models.py
+++ foss_nanogrid/optimization/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Model for ESS sytems: Probably Batteries
+class ESS(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=250)
+    capacity = models.FloatField(verbose_name="Capacity") # in MWh
+    max_power = models.FloatField(verbose_name="Max Power") # in MW
+    efficiency = models.FloatField(verbose_name="Efficiency") # in percentage
+    rte = models.FloatField(verbose_name="Round Trip Efficiency") # in percentage
+    sd = models.FloatField(verbose_name="Self Discharge") # in percentage per hour
+    dd = models.FloatField(verbose_name="Depth of Discharge") # in percentage
